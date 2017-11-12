31a7b938ecd921057e19c20b7d0f7f95d3b6a3fc
Zoran Zaric <zz@zoranzaric.de>
2017-11-12 11:07:24 +0100

    Reuse client

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,7 @@ use futures::stream::Stream;
 use hyper::{Client, Method, Request};
 use hyper::header::{ContentLength, ContentType};
 
-fn request(good: bool) {
-    let url = match env::args().nth(1) {
-        Some(url) => url,
-        None => {
-            println!("Usage: client <url>");
-            return;
-        }
-    };
-
-    let url = url.parse::<hyper::Uri>().unwrap();
-    if url.scheme() != Some("http") {
-        println!("This example only works with 'http' URLs.");
-        return;
-    }
-
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
-    let client = Client::configure().build(&handle);
-
+fn request(url: hyper::Uri, good: bool) -> Request {
     let json = if good {
         r#"{"id": "bar", "event_type": {"id": 6}}"#
     } else {
@@ -46,29 +28,66 @@ fn request(good: bool) {
     request.headers_mut().set(ContentLength(json.len() as u64));
     request.set_body(json);
 
-    let work = client.request(request);
-
-    //    .and_then(|res| {
-    //        println!("Response: {}", res.status());
-    //        println!("Headers: \n{}", res.headers());
-    //
-    //        res.body().for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
-    //    })
-    //    .map(|_| {
-    //        //            println!("\n\nDone.");
-    //    });
-
-    core.run(work).unwrap();
+    request
 }
 
 fn rand(mut rng: ThreadRng) -> bool {
     let r = rng.gen::<u32>();
 
-    r % 10 == 0
+    r % 10000 == 0
 }
 
 fn main() {
-    let rng = rand::thread_rng();
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+    let client = Client::configure().build(&handle);
 
-    request(rand(rng));
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Usage: client <url>");
+            return;
+        }
+    };
+
+    if url.parse::<hyper::Uri>().unwrap().scheme() != Some("http") {
+        println!("This example only works with 'http' URLs.");
+        return;
+    }
+
+
+
+    let mut c = 0;
+    let mut r = request(url.parse::<hyper::Uri>().unwrap(), true);
+
+    loop {
+        // let rng = rand::thread_rng();
+        // request(rand(rng));
+        if c % 10000 == 0 {
+            r = request(url.parse::<hyper::Uri>().unwrap(), true);
+        } else {
+            r = request(url.parse::<hyper::Uri>().unwrap(), false);
+        }
+
+        let work = client.request(r);
+
+        //    .and_then(|res| {
+        //        println!("Response: {}", res.status());
+        //        println!("Headers: \n{}", res.headers());
+        //
+        //        res.body().for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+        //    })
+        //    .map(|_| {
+        //        //            println!("\n\nDone.");
+        //    });
+
+        core.run(work).unwrap();
+
+        c = c + 1;
+
+
+        if c % 1000 == 0 {
+            println!("{}", c);
+        }
+    }
 }
